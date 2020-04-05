59306fe9cf4955ceacd214f6a8b587a238bd0113
Silvia Rocha <silviadprocha@gmail.com>
2020-04-05 16:09:43 +0900

    add style

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import flats from "../../data/flat";
+import Flat from "./flat";
+
+export class app extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default app;
